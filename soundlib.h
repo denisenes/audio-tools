a84aed0a07c50c03ade47b34f60177d85d5b9cbd
Denis Enes <55022112+denisenes@users.noreply.github.com>
2020-07-08 00:09:59 +0700

    Update soundlib.h

--- soundlib.h
+++ soundlib.h
@@ -63,6 +63,10 @@ typedef struct WavHeader_m
 
 } WavHeader;
 
+//  [            Sample            ]
+//  [   channel1   ][   channel2   ]
+//  [b1][b2][b3][b4][b1][b2][b3][b4]
+
 // Here we contain header and data(array of integers) from wav file
 typedef struct wavFile_m {
     WavHeader header;
@@ -314,6 +318,54 @@ WavFile * mixer(int64_t N, ...) {
     }
     va_end(waves);
     return newWave;
+}
+
+//========================================================
+//                    Filters
+//========================================================
+
+// simple linear ADSR volume envelope (mono)
+// 0 <= a, d, s, r <= 100 (in percents)
+// 0 <= susVal <= 1
+void ADSR(WavFile * wave, double attack, double decay, double sustain, double susVal, double release) {
+    //check arguments
+    if (attack + decay + sustain + release != 100) { //mega cool condition :3
+        return;
+    }
+    if (susVal < 0 || susVal > 1) {
+        return;
+    }
+
+    int size = wave->header.subchunk2Size / (wave->header.bitsPerSample / 8);
+    // build envelope wave
+    //freopen("log.txt", "w", stdout);
+    double envelopeWave[size];
+    for (int i = 0; i < size; i++) {
+        envelopeWave[i] = 1;
+    }
+    double attackLength = size / 100 * attack;
+    for (int i = 0; i < attackLength; i++) { //attack zone
+        envelopeWave[i] = (1 * i / attackLength);
+        //printf("%lf\n", envelopeWave[i]);
+    }
+    double decayLength = size / 100 * decay;
+    for (int i = attackLength; i < attackLength + decayLength; i++) {
+        envelopeWave[i] = (susVal-1)*(i - attackLength)/decayLength + 1;
+    }
+    double sustainLength = size / 100 * sustain;
+    for (int i = attackLength + decayLength; i < attackLength + decayLength + sustainLength; i++) {
+        envelopeWave[i] = susVal;
+    }
+    double releaseLength = size / 100 * release;
+    for (int i = attackLength + decayLength + sustainLength; i < attackLength + decayLength + sustainLength + releaseLength; i++) {
+        envelopeWave[i] = (-susVal *(i - (attackLength + decayLength + sustainLength)))/releaseLength + susVal;
+        //printf("%lf\n", envelopeWave[i]);
+    }
+    
+    // modulate amplitude of the input wave using the envelope wave
+    for (int i = 0; i < size; i++) {
+        wave->data[i].dMode32 = (int32_t) ((double) wave->data[i].dMode32 * envelopeWave[i]);
+    }
 }
 
 #endif
